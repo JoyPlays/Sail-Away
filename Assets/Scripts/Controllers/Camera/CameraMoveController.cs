73c15a073b55a1678fa205a1046304abed25729c
igorsboldovs <igors@estoty.com>
2020-08-26 22:31:10 +0300

    *Camera controller WIP, Camera movement added with WASD and arrow keays

---
+++ Assets/Scripts/Controllers/Camera/CameraMoveController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraMoveController : MonoBehaviour, IUpdateable
+{
+	[SerializeField] private Transform freeLookCameraTransform;
+
+	private float _currentMoveDistance = 50f;
+	
+	private const float MinMoveDistance = 50f;
+	private const float MaxMoveDistance = 100f;
+	private const float MoveDistanceChangeStep = 10f;
+	
+	private void Start()
+	{
+		if (UpdateController.Instance)
+		{
+			UpdateController.Instance.RegisterUpdateableObject(this);
+		}
+	}
+	
+	private void OnDestroy()
+	{
+		if (UpdateController.Instance)
+		{
+			UpdateController.Instance.DeregisterUpdateableObject(this);
+		}
+	}
+
+	public void OnUpdate(float deltaTime)
+	{
+		float horizontalAxis = Input.GetAxis("Horizontal");
+		float verticalAxis = Input.GetAxis("Vertical");
+		Vector3 pos = freeLookCameraTransform.position;
+		
+		if (Math.Abs(horizontalAxis) > 0.001f || Math.Abs(verticalAxis) > 0.001f)
+		{
+			pos.x += horizontalAxis;
+			pos.z += verticalAxis;
+			_currentMoveDistance += deltaTime * MoveDistanceChangeStep;
+		}
+		else if (_currentMoveDistance > MinMoveDistance)
+		{
+			_currentMoveDistance = MinMoveDistance;
+		}
+
+		_currentMoveDistance = Mathf.Clamp(_currentMoveDistance, MinMoveDistance, MaxMoveDistance);
+		Debug.Log(_currentMoveDistance);
+		
+		freeLookCameraTransform.position = Vector3.MoveTowards(freeLookCameraTransform.position, pos, _currentMoveDistance * deltaTime);
+	}
+}
---
+++ Assets/Scripts/Controllers/UpdateController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateController : MonoBehaviour
+{
+	private List<IUpdateable> _updateableObjects = new List<IUpdateable>();
+	
+	private static UpdateController _instance;
+	public static UpdateController Instance
+	{
+		get => _instance;
+		private set => _instance = value;
+	}
+
+	private void Awake()
+	{
+		Instance = this;
+	}
+
+	public void RegisterUpdateableObject(IUpdateable obj)
+	{
+		if (!_updateableObjects.Contains(obj))
+		{
+			_updateableObjects.Add(obj);
+		}
+	}
+
+	public void DeregisterUpdateableObject(IUpdateable obj)
+	{
+		if (_updateableObjects.Contains(obj))
+		{
+			_updateableObjects.Remove(obj);
+		}
+	}
+
+	void Update()
+	{
+		float dt = Time.deltaTime;
+		for (int i = 0; i < _updateableObjects.Count; ++i)
+		{
+			_updateableObjects[i].OnUpdate(dt);
+		}
+	}
+}
