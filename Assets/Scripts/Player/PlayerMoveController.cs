0f787af1f6d1fedf0f2b310a4abff14d4050e5ad
igorsboldovs <igors@estoty.com>
2020-10-12 20:36:55 +0300

    *Added weapon test scene, for further weapon implementation, implemented playr ship movement by mouse click

---
+++ Assets/Scripts/Player/PlayerMoveController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour, IUpdateable
+{
+	[SerializeField] private Camera gameCamera;
+	[SerializeField] private Transform playerMovePoint;
+	[SerializeField] private LayerMask rayMask;
+	
+	private void Start()
+	{
+		if (UpdateController.Instance)
+		{
+			UpdateController.Instance.RegisterUpdateableObject(this);
+		}
+	}
+	
+	private void OnDestroy()
+	{
+		if (UpdateController.Instance)
+		{
+			UpdateController.Instance.DeregisterUpdateableObject(this);
+		}
+	}
+	
+	public void OnUpdate(float deltaTime)
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out RaycastHit hit, 1000, rayMask))
+			{
+				playerMovePoint.position = hit.point;
+			}
+		}
+	}
+}
