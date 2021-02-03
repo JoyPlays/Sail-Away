b5bef5ce85e93afee06d465df4d59e362f4619eb
igorsboldovs <igors@estoty.com>
2021-02-03 21:16:22 +0200

    *Change camera type to ortographics and updated zoom functionallity

--- Assets/Scripts/Camera/CameraZoomController.cs
+++ Assets/Scripts/Camera/CameraZoomController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraZoomController : MonoBehaviour, IUpdateable
@@ -8,7 +9,9 @@ public class CameraZoomController : MonoBehaviour, IUpdateable
 	
 	[SerializeField] private CameraDataSO cameraData;
 
-	private float _currentZoomValue = 0f;
+	[SerializeField] private CinemachineVirtualCamera currentVirtualCamera;
+
+	private float _currentZoomValue = 50f;
 
 	private void Start()
 	{
@@ -30,15 +33,32 @@ public class CameraZoomController : MonoBehaviour, IUpdateable
 	{
 		float scrollAxis = Input.GetAxis("Mouse ScrollWheel");
 		
+		#region Ortographic Zoom
+
 		if (Mathf.Abs(scrollAxis) > 0.001f)
 		{
-			_currentZoomValue += scrollAxis * cameraData.zoomStep;
-			_currentZoomValue = Mathf.Clamp(_currentZoomValue, cameraData.maxZoomOutValue, cameraData.maxZoomInValue);
+			_currentZoomValue -= scrollAxis * cameraData.zoomStep;
+			_currentZoomValue = Mathf.Clamp(_currentZoomValue, cameraData.maxZoomInValue, cameraData.maxZoomOutValue);
 		}
 		
-		Vector3 zoomPosition = cameraTransform.forward * _currentZoomValue;
-		Vector3 newPosition = Vector3.MoveTowards(cameraTransform.localPosition, zoomPosition, cameraData.zoomDelta * deltaTime);
+		float currentSize = currentVirtualCamera.m_Lens.OrthographicSize;
+		float newSize = Mathf.MoveTowards(currentSize, _currentZoomValue, cameraData.zoomDelta * deltaTime);
+		currentVirtualCamera.m_Lens.OrthographicSize = newSize;
+
+		#endregion
+
+		#region Perspective Zoom
+
+		// if (Mathf.Abs(scrollAxis) > 0.001f)
+		// {
+		// 	_currentZoomValue += scrollAxis * cameraData.zoomStep;
+		// 	_currentZoomValue = Mathf.Clamp(_currentZoomValue, cameraData.maxZoomOutValue, cameraData.maxZoomInValue);
+		// }
 		
-		cameraTransform.localPosition = newPosition;
+		// Vector3 zoomPosition = cameraTransform.forward * _currentZoomValue;
+		// Vector3 newPosition = Vector3.MoveTowards(cameraTransform.localPosition, zoomPosition, cameraData.zoomDelta * deltaTime);
+		// cameraTransform.localPosition = newPosition;
+
+		#endregion
 	}
 }
