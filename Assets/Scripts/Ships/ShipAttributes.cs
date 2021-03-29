c2d0b3e83885b2993ed9d9bbf81fdfa0030f27ce
igorsboldovs <igors@estoty.com>
2021-03-29 22:52:30 +0300

    *Ship System wip

---
+++ Assets/Scripts/Ships/ShipAttributes.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ship Attributes", menuName = "Ships/Ship Attributes")]
+public class ShipAttributes : ScriptableObject
+{
+	#region Fields
+	
+	[SerializeField] private string shipName;
+	[SerializeField] private int cargoHold;
+	[SerializeField] private float turnRate;
+	[SerializeField] private float speed;
+	[SerializeField] private int hitPoint;
+	[SerializeField] private int viewRange;
+	
+	#endregion
+	
+	#region Properties
+	
+	public string ShipName => shipName;
+
+	public int CargoHold => cargoHold;
+
+	public float TurnRate => turnRate;
+
+	public float Speed => speed;
+
+	public int HitPoints => hitPoint;
+
+	public int ViewRange => viewRange;
+	
+	#endregion
+}
---
+++ Assets/Scripts/Ships/ShipData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName  = "Ship Data", menuName = "Ships/Ship Data")]
+public class ShipData : ScriptableObject
+{
+	[SerializeField] private ShipFacade shipPrefab;
+	[SerializeField] private ShipAttributes shipAttributes;
+}
